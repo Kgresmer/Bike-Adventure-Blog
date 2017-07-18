db503ca4d4056365578b37ab709d9057c8d90923
Kgresmer <kgresmer@gmail.com>
2017-07-18 13:58:59 -0700

    Added a navbar as well as JQuery and Bootstrap

--- Bike-Blog-New/angular-src/src/app/app.module.ts
+++ Bike-Blog-New/angular-src/src/app/app.module.ts
@@ -1,14 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
-import { PostComponent } from './components/post/post.component';
+import {PostComponent} from './components/post/post.component';
 import {PostDetailComponent} from './components/post-detail/post-detail.component';
 import {AppComponent} from './app.component';
 import {PostService} from './components/post/post.service';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {AboutComponent} from './components/about/about.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {HomeComponent} from './components/home/home.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {ProfileComponent} from './components/profile/profile.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'posts',
+    component: PostComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }];
 
 @NgModule({
   declarations: [
@@ -16,27 +51,24 @@ import {AboutComponent} from './components/about/about.component';
     PostDetailComponent,
     AppComponent,
     NavbarComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'posts',
-        component: PostComponent
-      },
-      {
-        path: 'about',
-        component: AboutComponent
-      }
-    ])
+    RouterModule.forRoot(appRoutes)
 
-],
+  ],
   providers: [
     PostService
   ],
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
--- Bike-Blog-New/angular-src/src/main.ts
+++ Bike-Blog-New/angular-src/src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'jquery/src/jquery';
+import 'bootstrap/dist/js/bootstrap';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
