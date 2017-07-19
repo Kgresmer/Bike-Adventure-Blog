47cf1e8996bfe8322bb82850f8fb1122ddee7a68
Kgresmer <kgresmer@gmail.com>
2017-07-19 16:13:45 -0700

    Added an auth service to login  and log out. Added saving user and token to local storage

--- Bike-Blog-New/angular-src/src/app/components/navbar/navbar.component.ts
+++ Bike-Blog-New/angular-src/src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,9 @@
  * Created by chq-keving on 7/14/2017.
  */
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -11,11 +14,23 @@ import {Component, OnInit} from '@angular/core';
 
 export class NavbarComponent implements OnInit {
 
-  constructor() {
+  constructor(private authService: AuthService,
+              private router: Router,
+              private flashMessagesService: FlashMessagesService) {
 
   }
 
   ngOnInit(): void {
+  }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessagesService.show('You have been logged out', {
+      cssClass: 'alert-success',
+      timeout: 3000
+    });
+    this.router.navigate(['/login']);
+    return false;
   }
 
 }
