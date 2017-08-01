4ae270bf6a362df0b3f4a2e82365cf44253feb2a
Kgresmer <kgresmer@gmail.com>
2017-08-01 13:15:38 -0700

    added a slash in the client side folder name

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) { //allow cross origin requests
 });
 
 //Set static folder
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '/client')));
 
 // Body parser Middleware
 app.use(bodyParser.json());
