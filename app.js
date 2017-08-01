53272332dc127fa475a72a14d0d1c604bc412bf1
Kgresmer <kgresmer@gmail.com>
2017-08-01 13:33:12 -0700

    added a slash in the client side folder name

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'client/index.html'));
+   res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
 //start server
--- client/styles.bundle.js
+++ client/styles.bundle.js
@@ -70,7 +70,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 exports.i(__webpack_require__("../../../../css-loader/index.js?{\"sourceMap\":false,\"importLoaders\":1}!../../../../postcss-loader/index.js?{\"ident\":\"postcss\"}!../../../../bootstrap/dist/css/bootstrap.css"), "");
 
 // module
-exports.push([module.i, "\r\n", ""]);
+exports.push([module.i, "\n", ""]);
 
 // exports
 
