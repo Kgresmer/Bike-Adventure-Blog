03b38e6e87df4ea245f59544d16b270fdf1e4011
Kevin <kgresmer@gmail.com>
2017-08-16 10:03:49 -0700

    Changed the appearance of feed and totals

--- angular-src/src/app/components/post/post.component.ts
+++ angular-src/src/app/components/post/post.component.ts
@@ -8,6 +8,7 @@ import {Observable} from "rxjs/Observable";
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
 })
 
 export class PostComponent implements OnInit {
