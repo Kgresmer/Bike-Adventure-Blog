b87f33a6be9d31c0304f45525059fd32c8ce12ed
kgresmer <kgresmer@gmail.com>
2017-08-22 09:57:21 -0700

    Visual changes to the calendar

--- angular-src/src/app/components/calendar/calendar.component.ts
+++ angular-src/src/app/components/calendar/calendar.component.ts
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {PostService} from "../../services/post.service";
 import {FlashMessagesService} from "angular2-flash-messages";
 
+declare const jQuery: any;
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -25,6 +27,12 @@ export class CalendarComponent {
     this.postService.getPostsByDate(dateChosen).subscribe(response => {
       if (response.success) {
         this.posts = response.posts;
+
+        if (this.posts.length > 0) {
+          jQuery('html, body').animate({
+            scrollTop: jQuery("#post-display").offset().top
+          }, 1000);
+        }
       } else {
         this.flashMessagesService.show('I\'m Sorry. I can\'t find any posts for that date. ', {
           cssClass: 'alert-danger',
